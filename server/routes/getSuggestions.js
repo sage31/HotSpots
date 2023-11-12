7d8096c4514894432c986497ea3fc8f6f26949b5
anisshan <anish.shanmug@gmail.com>
2023-11-12 00:03:41 -0800

    calculateNetArea() made
    also installed turf.js and geoJson-area

--- server/routes/getSuggestions.js
+++ server/routes/getSuggestions.js
@@ -1,5 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const turf = require('turf');
+const geojsonArea = require('geojson-area');
+
+function calculateNetArea(geoJsonPolygons) {
+    let union;
+    geoJsonPolygons.features.forEach(polygon => {
+        if (!union) {
+            union = polygon;
+        } else {
+            union = turf.union(union, polygon);
+        }
+    });
+    // Calculate the total area
+    const totalArea = geojsonArea.geometry(union.geometry);
+
+    return totalArea;
+}
+
+
 
 router.get("/", async (req, res) => {
 
